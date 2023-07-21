79b2ebfae58ae0a2d0a96494745f34caf57825ac
ZahrenShahh <128072580+ZahrenShahh@users.noreply.github.com>
2023-07-21 15:46:01 +0800

    Add files via upload

---
+++ question01/tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      mPlus: ["Montserrat","Sans-serif"],
+      smplus: ["Space Mono","monospace"],
+      exo: ["Exo","sans-serif"],
+      Unica: ["Unica One","cursive"],
+
+  },
+  },
+  plugins: [],
+};
